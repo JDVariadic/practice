843f4d4cd9cb4ed777144c49fd0d3befc3dab2a8
Jared Matthew P. De Guzman <jartalk@gmail.com>
2019-07-30 15:56:09 +0800

    feat(phrase-highlight.js): added three-word phrase
    the program supports both two and three-word phrases

--- phrase-highlight.js
+++ phrase-highlight.js
@@ -1,5 +1,6 @@
-const phrase = 'boy roger'
-const sentence = 'boy boy roger roger all the good neighbors have brought their boy to teach good them to ride a good bike at the park today. they keep tumbling around but they are okay. but The good boy that picked the litter is the one named roger! roger may not be good at riding the bike, but he has good manners.'
+const phrase = 'good boy'
+let sentence = 'All the good neighbors have brought their boy to teach them to ride a bike at the park today. They keep tumbling around but they are okay. But The good boy that picked good boy roger the litter is the one named Roger! Roger may not be good at riding the bike, but he has good manners.'
+sentence = sentence.toLowerCase()
 
 function searchForPhrase (inputPhrase, inputSentence) {
   const arrayOfPhrase = inputPhrase.split(' ')
@@ -13,45 +14,100 @@ function searchForPhrase (inputPhrase, inputSentence) {
   let wordA
   let backupA
   let wordB
-  // let wordBackupB
-  // let wordC
 
-  for (const wordInSentenceIndex in arrayOfSentence) {
+  // Optional variables depending on whether or not the phrase contains two or three words
+  let backupB
+  let wordC
 
-    if (arrayOfSentence[wordInSentenceIndex] === arrayOfPhrase[0]) {
+  if (arrayOfPhrase.length === 3) {
+    for (const wordInSentenceIndex in arrayOfSentence) {
 
-      if (typeof (wordA) === 'undefined') {
-        wordA = wordInSentenceIndex
-      } else if (typeof (wordA) !== 'undefined') {
-        if (typeof (wordB) === 'undefined') {
-          backupA = wordInSentenceIndex
+      if (arrayOfSentence[wordInSentenceIndex].replace(/[^\w\s]|_/g, '') === arrayOfPhrase[0]) {
+        // check whether or not wordA exists and act accordingly
+        if (typeof (wordA) === 'undefined') {
+          wordA = wordInSentenceIndex
+        } else if (typeof (wordA) !== 'undefined') {
+          if (typeof (wordB) === 'undefined') {
+            backupA = wordInSentenceIndex
+            wordA = backupA
+          } else {
+            backupA = wordInSentenceIndex
+          }
+        }
+
+      } else if (arrayOfSentence[wordInSentenceIndex].replace(/[^\w\s]|_/g, '') === arrayOfPhrase[1]) {
+        // Check whether or not wordB and wordA exists and act accordingly
+        // Instances when backupB traverses but is not assigned to wordB
+        // Instances when both backupB and wordB change (through calculating distances)
+        if (typeof (wordA) !== 'undefined') {
+          if (typeof (wordB) === 'undefined' || (backupB - backupA > 0 && wordB - wordA > backupB - backupA)) {
+            wordB = wordInSentenceIndex
+            backupB = wordB
+          } else if (typeof (wordB) !== 'undefined') {
+            backupB = wordInSentenceIndex
+          }
+        }
+
+      } else if (arrayOfSentence[wordInSentenceIndex].replace(/[^\w\s]|_/g, '') === arrayOfPhrase[2]) {
+
+        if (typeof wordC === 'undefined' && typeof wordB !== 'undefined' && typeof wordA !== 'undefined') {
+          wordC = wordInSentenceIndex
+        }
+        // Check if new wordC (wordInSentenceIndex) is nearer to backupB than wordC is to wordB
+        if (wordInSentenceIndex - backupB > 0 && wordInSentenceIndex - backupB < wordC - wordB) {
+          wordC = wordInSentenceIndex
+          wordB = backupB
           wordA = backupA
-        } else if (typeof (wordB) !== 'undefined') {
-          backupA = wordInSentenceIndex
         }
       }
-
-    } else if (arrayOfSentence[wordInSentenceIndex] === arrayOfPhrase[1]) {
-
-      if (typeof (wordA) !== 'undefined' && typeof (wordB) === 'undefined') {
-        wordB = wordInSentenceIndex
-      }
-
-      // For 3-word
-      // if (Math.abs(wordB - wordA) < Math.abs(wordB - backupA)) {
-      //   backupA = wordA
-      // }
     }
+  } else if (arrayOfPhrase.length === 2) {
+    for (const wordInSentenceIndex in arrayOfSentence) {
+      if (arrayOfSentence[wordInSentenceIndex] === arrayOfPhrase[0]) {
+
+        if (typeof (wordA) === 'undefined') {
+          wordA = wordInSentenceIndex
+        } else if (typeof (wordA) !== 'undefined') {
+          if (typeof (wordB) === 'undefined') {
+            backupA = wordInSentenceIndex
+            wordA = backupA
+          } else if (typeof (wordB) !== 'undefined') {
+            backupA = wordInSentenceIndex
+          }
+        }
+
+      } else if (arrayOfSentence[wordInSentenceIndex] === arrayOfPhrase[1]) {
+
+        if (typeof (wordA) !== 'undefined' && typeof (wordB) === 'undefined') {
+          wordB = wordInSentenceIndex
+        }
+
+        if (wordInSentenceIndex - backupA > 0 && wordInSentenceIndex - backupA < wordB - wordA) {
+          wordA = backupA
+          wordB = wordInSentenceIndex
+        }
+
+        // For 3-word
+        // if (Math.abs(wordB - wordA) < Math.abs(wordB - backupA)) {
+        //   backupA = wordA
+        // }
+      }
+    }
+  } else {
+    return 'invalid amount of words in the phrase'
   }
 
   toBeHighlighted.push(wordA)
   toBeHighlighted.push(wordB)
+  if (typeof wordC !== 'undefined') {
+    toBeHighlighted.push(wordC)
+  }
+  console.log(toBeHighlighted)
   for (const index in toBeHighlighted) {
+    console.log(toBeHighlighted[index])
     arrayOfSentence[toBeHighlighted[index]] = '<p>' + arrayOfSentence[toBeHighlighted[index]] + '<p>'
   }
-  console.log('wordA', wordA)
-  console.log('backupA', backupA)
-  console.log('wordB', wordB)
+
   arrayOfSentence = arrayOfSentence.join(' ')
   return arrayOfSentence
 }
